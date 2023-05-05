bd508bc58383a7ee0821596da39401cd1a0a3efa
shikhar190399 <87176255+shikhar190399@users.noreply.github.com>
2023-05-05 21:59:36 +0530

    Update config.js

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,2 +1,2 @@
 const mongoose= require('mongoose')
-mongoose.connect(process.env.MONGO_URI, console.log("MongoDb connected")
+mongoose.connect(process.env.MONGO_URI, console.log("MongoDb connected"))
